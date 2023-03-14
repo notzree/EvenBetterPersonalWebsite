9f23488f2b0fc824ef1a130893b781292faeb7e1
Richard <notzree@gmail.com>
2023-03-13 23:57:54 -0400

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Toaster } from 'react-hot-toast'
-
+import { Analytics } from '@vercel/analytics/react';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
     <Toaster/>
     <Component {...pageProps} />
+    <Analytics />
     </>
   
   )
