44b52d4945bc1c5ec7c23a5edf3f9ceaab73c1bb
Richard Zhang <notzree@gmail.com>
2023-10-10 09:29:52 -0400

    accordian + new ui script

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,7 @@
-export default function Footer(){
-
-
-    return(
-        <div className="flex justify-center items-center mb-10">
-        <h1 className="text-primary">&copy; Richard Zhang 2023</h1>
-      </div>
-
-    )
+export default function Footer() {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 flex justify-center items-center mb-10">
+      <h1 className="text-primary">&copy; Richard Zhang 2023</h1>
+    </div>
+  );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,12 +4,17 @@ import {
   AiOutlineMail,
 } from "react-icons/ai";
 import { useState, useEffect } from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 import { toast } from "react-hot-toast";
 import { Separator } from "./ui/separator";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./theme-toggle";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 export default function Hero() {
   function copyEmail() {
     const text = "r29zhang@uwaterloo.ca";
@@ -39,7 +44,7 @@ export default function Hero() {
 
   return (
     <div className=" bg-backgroundmin-h-screen w-screen flex flex-col items-center px-10 md:px-0 mt-12">
-      <div className="flex flex-col justify-between items-start md:w-auto">
+      <div className="flex flex-col justify-between items-start w-auto md:w-[35rem]">
         <div className="flex justify-between items-center mb-6 w-full">
           <div className="space-y-1">
             <div className="my-2 flex h-5 items-center space-x-2 ">
@@ -87,35 +92,57 @@ export default function Hero() {
           </div>
         </div>
         <div className="w-full ">
-          <h3 className="font-mada font-bold text-2xl  text-primary underline decoration-muted-foreground  underline-offset-8 decoration-4 mb-6">
-            &nbsp;about &nbsp;
-          </h3>
-          <p className=" text-primary md:w-[36rem] text-md md:text-lg mb-8  text-left">
-            Richard is a software engineer based in Toronto studying Systems
-            Design Engineering at the{" "}
-            <span className="text-green-400">University of Waterloo. </span>
-            He is working as a Fullstack Engineer at{" "}
-            <a
-              href="https://www.getjitto.com/"
-              className="text-green-400 underline"
-              target="_blank"
-              rel="norefferer"
-            >
-              Jitto
-            </a>
-            ,&nbsp;where he is currently migrating their database from DynamoDB
-            to Aurora ServerlessV2. He is most familiar with react.js, node.js,
-            and python but is secretly wants to be a golang developer. From his
-            current job, he&apos;s learned that he loves working at startups and
-            is currently
-            <a className="text-green-400 underline">
-              &nbsp;looking for co-op opportunities for Summer 2024
-            </a>
-            .&nbsp;Aside from work, Richard is addicted to Muay Thai and is
-            learning how to cook. He is also working towards his AWS certified
-            developer certification.
-          </p>
-          <div className="flex justify-center items-center mb-2"></div>
+          <Accordion
+            type="single"
+            collapsible
+            className="w-full"
+            defaultValue="about"
+          >
+            <AccordionItem value="about">
+              <AccordionTrigger>about</AccordionTrigger>
+              <AccordionContent>
+                Richard is a software engineer based in Toronto studying Systems
+                Design Engineering at the{" "}
+                <span className="text-muted-foreground">
+                  University of Waterloo.{" "}
+                </span>
+                He is working as a Fullstack Engineer at{" "}
+                <a
+                  href="https://www.getjitto.com/"
+                  className=" text-muted-foreground underline"
+                  target="_blank"
+                  rel="norefferer"
+                >
+                  Jitto
+                </a>
+                ,&nbsp;where he is currently migrating their database from
+                DynamoDB to Aurora ServerlessV2. He is most familiar with
+                react.js, node.js, and python but is secretly wants to be a
+                golang developer. From his current job, he&apos;s learned that
+                he loves working at startups and is currently
+                <a className="text-muted-foreground underline">
+                  &nbsp;looking for co-op opportunities for Summer 2024
+                </a>
+                .&nbsp;Aside from work, Richard is addicted to Muay Thai and is
+                learning how to cook. He is also working towards his AWS
+                certified developer certification.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="portfolio">
+              <AccordionTrigger>portfolio</AccordionTrigger>
+              <AccordionContent>
+                Yes. It comes with default styles that matches the other
+                components&apos; aesthetic.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="resume">
+              <AccordionTrigger>resume</AccordionTrigger>
+              <AccordionContent>
+                Yes. It&apos;s animated by default, but you can disable it if
+                you prefer.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
       </div>
       <div className="md:w-[70rem] md:h-[30rem] flex items-center justify-center mb-10 w-[20rem] h-[40rem] "></div>
